bd0b0530050802dd9a43522a448fb5e0cfbcaddd
Bilonenko Roman <bra1905@gmail.com>
2019-11-02 12:45:39 +0200

    Initial commit

---
+++ src/main/java/com/example/registershares/RegisterSharesApplication.java
@@ -0,0 +1,13 @@
+package com.example.registershares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterSharesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterSharesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/registershares/RegisterSharesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registershares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterSharesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
